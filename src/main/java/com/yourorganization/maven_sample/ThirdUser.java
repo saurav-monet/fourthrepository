f351c79e8028196fcddcfee3bd72294e53fae48c
saurav-monet <saurav.kumar@monetnetworks.com>
2021-08-18 17:18:32 +0530

    5th Commit

--- src/main/java/com/yourorganization/maven_sample/ThirdUser.java
+++ src/main/java/com/yourorganization/maven_sample/ThirdUser.java
@@ -20,7 +20,7 @@ public class ThirdUser
 
 	     ChromeOptions options = new ChromeOptions();
 		 options.addArguments("--use-fake-ui-for-media-stream");
-		 System.setProperty("webdriver.chrome.driver","/usr/bin/google-chrome");
+		 System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
 		 //System.setProperty("webdriver.chrome.driver","C:\\WebDriver\\bin\\chromedriver.exe");
 		 options.addArguments("--headless");
 	    options.addArguments("--use-fake-device-for-media-stream");
